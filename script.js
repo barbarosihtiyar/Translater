0cea9f10dc08e9291a64c49b0a4df50aec61d79f
darkmar61 <104565169+darkmar61@users.noreply.github.com>
2022-08-20 00:08:38 +0300

    click-translate

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-const fromLang = document.getElementById("from-lang");
-const toLang   = document.getElementById("to-lang");
+const fromLang      = document.getElementById("from-lang");
+const toLang        = document.getElementById("to-lang");
+const btnTranslate  = document.getElementById("btnTranslate");
+const fromText      = document.getElementById("from-text");
+const toText        = document.getElementById("to-text");
+const exchange      = document.getElementById("exchange");
+const icons         = document.querySelectorAll(".icons");
 
 for(let lang in languages){
-    // console.log(lang,languages[lang]);
 
     let options = `<option value=${lang}>${languages[lang]}</option>`;
     fromLang.insertAdjacentHTML("beforeend",options);
@@ -12,3 +16,16 @@ for(let lang in languages){
     toLang.value   = "en-GB";
 
 }
+
+btnTranslate.addEventListener("click",() =>{
+
+    let text = fromText.value;
+    let from = fromLang.value;
+    let to   = toLang.value;
+    const url = `https://api.mymemory.translated.net/get?q=${text}&langpair=${from}|${to}`;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => toText.value = data.responseData.translatedText);
+});
+
